5816f445398f8991537e9ea64f65271ec5ed9574
fduriez <f.duriez@hotmail.fr>
2017-02-09 21:05:30 +0100

    Separation des fichiers

---
+++ src/compare.js
@@ -0,0 +1,40 @@
+exports.Compare = function (realty, agent) {
+	var price = realty.price.replace(/\s/g,"");
+    price = parseInt(price);
+    var area = parseInt(realty.area);
+    var areaPrice = price/area;
+    
+    console.log("price by m² : " + areaPrice);
+    
+    var lowPrice = agent.lowPrice.replace(/\s/g,"");
+    lowPrice =parseInt(lowPrice);
+    var averagePrice = agent.averagePrice.replace(/\s/g,"");
+    averagePrice =parseInt(averagePrice);
+    var highPrice = agent.highPrice.replace(/\s/g,"");
+    highPrice =parseInt(highPrice);
+
+    console.log(lowPrice);
+    console.log(averagePrice);
+    console.log(highPrice);
+
+    if(areaPrice < lowPrice) {
+    	agent.dealType = "Very Good Deal";
+    }
+    else if(areaPrice < lowPrice + (averagePrice - lowPrice)*(2/3)) {
+    	agent.dealType = "Good Deal";
+    }
+    else if(areaPrice < averagePrice + (highPrice - averagePrice)*(1/3)) {
+    	agent.dealType = "Reasonable Deal";
+    }
+    else if(areaPrice < highPrice) {
+    	agent.dealType = "Bad Deal";
+    }
+    else if(areaPrice > highPrice) {
+    	agent.dealType = "Very Bad Deal";
+    }
+    else {
+    	agent.dealType = "Error Calcul";
+    }
+
+    console.log(agent.dealType);
+}
---
+++ src/scrapeMA.js
@@ -0,0 +1,98 @@
+var compareFile = require('./compare.js');
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.MeilleursAgents = function (url, realty, agent) {
+    console.log('******************* Début accès Meilleurs Agents *************************');
+    console.log('je suis dans ma fonction');
+    console.log('url : ');
+    console.log(url);
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            if(realty.type == "Maison") {
+                //Prix Bas m²
+                $('div.medium-uncollapse:nth-child(3) > div:nth-child(2)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.lowPrice = price;
+                })
+
+                //Prix Moyen m²
+                $('div.medium-uncollapse:nth-child(3) > div:nth-child(3)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.averagePrice = price;
+                })
+
+                //Prix Haut m²
+                $('div.medium-uncollapse:nth-child(3) > div:nth-child(4)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.highPrice = price;
+                })
+            }
+            else if(realty.type == "Appartement") {
+                //Prix Bas m²
+                $('div.medium-uncollapse:nth-child(2) > div:nth-child(2)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.lowPrice = price;
+                })
+                
+                //Prix Moyen m²
+                $('div.medium-uncollapse:nth-child(2) > div:nth-child(3)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.averagePrice = price;
+                })
+
+                //Prix Haut m²
+                $('div.medium-uncollapse:nth-child(2) > div:nth-child(4)').filter(function(){
+                    var data = $(this);
+                    var price = data.text();
+                    while (price[0] < 1 || price[0] >10){
+                        price = price.slice(1);
+                    }
+                    price = price.slice(0, price.indexOf("€") + 1);
+                    agent.highPrice = price;
+                })
+            }
+            console.log('Resultat : ');
+            console.log(agent);
+            console.log('********************************************');
+        }
+        compareFile.Compare(realty, agent);
+    })
+    
+}
+
