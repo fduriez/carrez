f0a66e6bf47b97ecb7c178ffafc56539c546a55a
fduriez <f.duriez@hotmail.fr>
2017-02-10 13:36:59 +0100

    page web

--- src/appWeb.js
+++ src/appWeb.js
@@ -1,8 +1,6 @@
-var maFile = require('./scrapeMA.js');
+var lbcFile = require('./scrapeLBC.js');
 
 var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
 var app     = express();
 
 var realty = {
@@ -18,106 +16,15 @@ var agent = {
     highPrice : 0 ,
     dealType : "" };
 
-console.log('****************** Début accès leboncoin **************************');
+app.use(express.static(__dirname + '/public'));
 
 url = 'https://www.leboncoin.fr/ventes_immobilieres/1087339301.htm?ca=12_s'; //Maison sans agence
 //url = 'https://www.leboncoin.fr/ventes_immobilieres/1052080390.htm?ca=12_s'; //Maison avec agence
 //url = 'https://www.leboncoin.fr/ventes_immobilieres/1058287543.htm?ca=12_s'; //Appartement sans agence
 //url = 'https://www.leboncoin.fr/ventes_immobilieres/1089921217.htm?ca=12_s'; //Appartement avec agence
 
-// The structure of our request call
-// The first parameter is our URL
-// The callback function takes 3 parameters, an error, response status code and the html
-request(url, function(error, response, html){
-
-    // First we'll check to make sure no errors occurred when making the request
-    if(!error){
-        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-        var $ = cheerio.load(html);
-
-        //Récupère le prix
-        $('span.value:nth-child(3)').filter(function(){
-            var price;
-            var data = $(this);
-            price = data.text();
-            while (price[0] < 1 || price[0] >10){
-                price = price.slice(1);
-            }
-            price = price.slice(0, price.indexOf("€") + 1);
-            realty.price = price;
-        })
-
-        //Récupère la ville et le code postal
-        $('div.line:nth-child(6) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
-            var city;
-            var ZIP_code;
-            var data = $(this);
-            city = data.text();
-            ZIP_code = city.slice(city.indexOf(" ") + 1, city.indexOf(" ") + 6);
-            city = city.slice(0, city.indexOf(" "));
-            realty.ZIP_code = ZIP_code;
-            realty.city = city;
-        })
-
-        //Récupère le type de bien 
-        $('div.line:nth-child(7) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
-            var type;
-            var data = $(this);
-            type = data.text();
-            realty.type = type;
-        })
-
-        //Si vendeur pro realtyType peut contenir les frais d'agence au lieu du type de bien
-        var pro = false;
-        if((realty.type != "Maison") && (realty.type != "Appartement")) {
-            $('div.line:nth-child(8) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
-                var type;
-                var data = $(this);
-                type = data.text();
-                realty.type = type;
-            })
-            pro = true;
-        }
-
-        //Si vendeur pro le conteneur de la surface est décalé
-        if(!pro) {
-            $('div.line:nth-child(9) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
-                var area;
-                var data = $(this);
-                area = data.text();
-                realty.area = area;
-            })
-        }
-        else if(pro) {
-            $('div.line:nth-child(10) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
-                var area;
-                var data = $(this);
-                area = data.text();
-                realty.area = area;
-            })
-        }
-
-        //Affiche les résultats
-        console.log('Résultat : ');
-        console.log(realty);
-
-        //Gère l'url MeilleursAgents 
-        url = 'https://www.meilleursagents.com/prix-immobilier/';
-        url += realty.city.toLowerCase();
-        url += "-";
-        url += realty.ZIP_code;
-
-        console.log(url);
-        console.log('********************************************');
-
-        maFile.MeilleursAgents(url, realty, agent);
-    }
-})
-
 app.listen('8080')
 console.log('Magic happens on port 8080');
 exports = module.exports = app;
 
-
-
+lbcFile.leboncoin(url, realty, agent);
---
+++ src/scrapeLBC.js
@@ -0,0 +1,112 @@
+var maFile = require('./scrapeMA.js');
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.leboncoin = function (url, realty, agent) {
+    console.log('****************** Début accès leboncoin **************************');
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            //Récupère le prix
+            $('span.value:nth-child(3)').filter(function(){
+                var price;
+                var data = $(this);
+                price = data.text();
+                while (price[0] < 1 || price[0] >10){
+                    price = price.slice(1);
+                }
+                price = price.slice(0, price.indexOf("€") + 1);
+                realty.price = price;
+            })
+
+            //Récupère la ville et le code postal
+            $('div.line:nth-child(6) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
+                var city;
+                var ZIP_code;
+                var data = $(this);
+                city = data.text();
+                ZIP_code = city.slice(city.indexOf(" ") + 1, city.indexOf(" ") + 6);
+                city = city.slice(0, city.indexOf(" "));
+                realty.ZIP_code = ZIP_code;
+                realty.city = city;
+            })
+
+            //Récupère le type de bien 
+            $('div.line:nth-child(7) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
+                var type;
+                var data = $(this);
+                type = data.text();
+                realty.type = type;
+            })
+
+            //Si vendeur pro realtyType peut contenir les frais d'agence au lieu du type de bien
+            var pro = false;
+            if((realty.type != "Maison") && (realty.type != "Appartement")) {
+                $('div.line:nth-child(8) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
+                    var type;
+                    var data = $(this);
+                    type = data.text();
+                    realty.type = type;
+                })
+                pro = true;
+            }
+
+            //Si vendeur pro le conteneur de la surface est décalé
+            if(!pro) {
+                $('div.line:nth-child(9) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
+                    var area;
+                    var data = $(this);
+                    area = data.text();
+                    realty.area = area;
+                })
+            }
+            else if(pro) {
+                $('div.line:nth-child(10) > h2:nth-child(1) > span:nth-child(2)').filter(function(){
+                    var area;
+                    var data = $(this);
+                    area = data.text();
+                    realty.area = area;
+                })
+            }
+
+            //Affiche les résultats
+            console.log('Résultat : ');
+            console.log(realty);
+
+            //Gère l'url MeilleursAgents 
+            url = 'https://www.meilleursagents.com/prix-immobilier/';
+            url += realty.city.toLowerCase();
+            url += "-";
+            url += realty.ZIP_code;
+
+            console.log(url);
+            console.log('********************************************');
+
+            maFile.MeilleursAgents(url, realty, agent);
+        }
+    })
+}
+
+/*var button = document.querySelector('#compute');
+
+button.addEventListener('click', function onClick () {
+    console.log("On y croit");
+    alert("alert");
+    var car = DRIVY.getCar();
+    var begin = document.querySelector('.begin').value;
+    var end = document.querySelector('.end').value;
+    var distance = document.querySelector('.distance').value;
+    var option = document.querySelector('.option').checked;
+
+    var actors = DRIVY.payActors(car, begin, end, distance, option);
+
+    render(actors);
+
+    return;
+});*/
